9c3a495a18e2f25a699227b0e195609f74be4d43
Syimyk Zaiyrbekov <zaiyrbekovsyimyk@gmail.com>
2022-02-28 15:45:43 +0600

    added files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // document.querySelector('select').addEventListener('input', function() {
-//   const value = this.value; 
+//   const value = this.value;
 //   document.querySelectorAll('ul').forEach(function(ul) {
 //     if(value === ul.id) {
 //       ul.className = '';
@@ -10,7 +10,6 @@
 //   });
 // });
 
-
 // (param1, param2, paramN) => expression
 // ES5
 // var add = function(x, y) {
@@ -18,7 +17,6 @@
 // };
 // ES6
 // let add = (x, y) => { return x + y };
-
 
 // document.querySelector('select').addEventListener('input', function() {
 //   document.querySelectorAll('ul').forEach(
@@ -39,7 +37,6 @@
 // document.body.classList.replace('hello', 'bye');
 
 // document.body.className = document.body.className === 'hello' ? '' : 'bye';
-
 
 // const articles = [
 //   {
@@ -80,51 +77,95 @@
 //   main.append(articleElement);
 // })
 
-const articles = [
+// const articles = [
+//   {
+//     id: "article-1",
+//     title: "Hello world",
+//     image: "./photo.jpg",
+//     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+//   },
+//   {
+//     id: "article-2",
+//     title: "Hello world",
+//     image: "./photo.jpg",
+//     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+//   },
+//   {
+//     id: "article-3",
+//     title: "Hello world",
+//     image: "./photo.jpg",
+//     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+//   }
+// ];
+
+// const main = document.querySelector('main');
+// articles.forEach(article => {
+//   const articleElement = document.createElement('article');
+//   const h1 = document.createElement('h1');
+//   const img = document.createElement('img');
+//   const p = document.createElement('p');
+
+//   articleElement.id = article.id;
+//   h1.textContent = article.title;
+//   img.src = article.image;
+//   p.textContent = article.body;
+
+//   articleElement.append(h1);
+//   articleElement.append(img);
+//   articleElement.append(p);
+//   main.append(articleElement);
+// })
+
+// // document.location.assign("")
+
+let fruist = ["apple", "banana",
+    false, 13,
+    "hello"["1", "2", "3"],
+    { name: "Peter", lastName: "Parcker" },
+    12345]
+fruist.forEach(e => console.log(typeof e));
+
+
+let people = [
   {
-    id: "article-1",
-    title: "Hello world",
-    image: "./photo.jpg",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    name: "Parker",
+    lastname: "Peter",
+    age: 34
   },
   {
-    id: "article-2",
-    title: "Hello world",
-    image: "./photo.jpg",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    name: "Laslay",
+    lastname: "Jhonson",
+    age: 55
   },
   {
-    id: "article-3",
-    title: "Hello world",
-    image: "./photo.jpg",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    name: "Sadyr",
+    lastname: "Japarov",
+    age: 44
+  },
+  {
+    name: "Elon",
+    lastname: "Mask",
+    age: 51
+  },
+  {
+    name: "Sam",
+    lastname: "Crus",
+    age: 11
   }
-];
 
-const main = document.querySelector('main');
-articles.forEach(article => {
-  const articleElement = document.createElement('article');
-  const h1 = document.createElement('h1');
-  const img = document.createElement('img');
-  const p = document.createElement('p');
+  
 
-  articleElement.id = article.id;
-  h1.textContent = article.title;
-  img.src = article.image;
-  p.textContent = article.body;
+]
 
-  articleElement.append(h1);
-  articleElement.append(img);
-  articleElement.append(p);
-  main.append(articleElement);
-})
+let container = document.querySelector("#container");
+people.forEach((item)=>{
+  let person = document.createElement("div");
+  let name = name.item;
+  let lastname = lastname.item;
+  let age = age.item;
 
-// document.location.assign("")
-
-
-
-
-
-
-
-
+  person.append(name);
+  person.append(lastname);
+  person.append(age);
+  container.append(person);
+});
